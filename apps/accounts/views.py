3c7b63518f1422fe4229dd16b75db85e5d0ce8e9
879372 <kaios5028@gmail.com>
2026-05-20 01:23:41 -0300

    feat: enable tenant-scoped AdminUserViewSet for company admins

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -3,7 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
-from core.permissions import IsSuperAdmin
+from core.permissions import IsCompanyAdmin
+from core.mixins import TenantScopedMixin
 
 class MeView(APIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -12,7 +13,13 @@ class MeView(APIView):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
 
-class AdminUserViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsSuperAdmin]
+class AdminUserViewSet(TenantScopedMixin, viewsets.ModelViewSet):
+    permission_classes = [IsCompanyAdmin]
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        if self.request.user.role != 'super_admin':
+            serializer.save(company=self.request.user.company)
+        else:
+            serializer.save()
