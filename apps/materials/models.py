b99e83488e7e8f9f9e5c7349812cddbf1a17b05b
879372 <kaios5028@gmail.com>
2026-06-10 23:23:07 -0300

    feat: restructure material orders to support nested suppliers and item pricing

--- apps/materials/models.py
+++ apps/materials/models.py
@@ -49,11 +49,21 @@ class MaterialOrder(models.Model):
     delivery_address = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+class MaterialOrderSupplier(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    order = models.ForeignKey(MaterialOrder, on_delete=models.CASCADE, related_name='suppliers')
+    name = models.CharField(max_length=200)
+    attachment_url = models.URLField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 class MaterialOrderItem(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    order = models.ForeignKey(MaterialOrder, on_delete=models.CASCADE, related_name='items')
+    supplier = models.ForeignKey(MaterialOrderSupplier, on_delete=models.CASCADE, related_name='items', null=True, blank=True)
     name = models.CharField(max_length=200)
-    quantity = models.CharField(max_length=100)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    unit_value = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return self.name
--- apps/materials/serializers.py
+++ apps/materials/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Material, MaterialOrder, MaterialOrderItem
+from .models import Material, MaterialOrder, MaterialOrderItem, MaterialOrderSupplier
 
 class MaterialSerializer(serializers.ModelSerializer):
     supplier_name = serializers.CharField(source='supplier.name', read_only=True)
@@ -18,34 +18,53 @@ class MaterialSerializer(serializers.ModelSerializer):
 class MaterialOrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MaterialOrderItem
-        fields = ['id', 'name', 'quantity']
+        fields = ['id', 'name', 'quantity', 'unit_value']
         read_only_fields = ['id']
 
-class MaterialOrderSerializer(serializers.ModelSerializer):
+class MaterialOrderSupplierSerializer(serializers.ModelSerializer):
     items = MaterialOrderItemSerializer(many=True)
 
     class Meta:
+        model = MaterialOrderSupplier
+        fields = ['id', 'name', 'attachment_url', 'items']
+        read_only_fields = ['id']
+
+class MaterialOrderSerializer(serializers.ModelSerializer):
+    suppliers = MaterialOrderSupplierSerializer(many=True)
+
+    class Meta:
         model = MaterialOrder
-        fields = ['id', 'company', 'project', 'description', 'status', 'expected_date', 'delivery_address', 'created_at', 'items']
+        fields = ['id', 'company', 'project', 'description', 'status', 'expected_date', 'delivery_address', 'created_at', 'suppliers']
         read_only_fields = ['id', 'created_at', 'company']
 
     def create(self, validated_data):
-        items_data = validated_data.pop('items', [])
+        suppliers_data = validated_data.pop('suppliers', [])
         order = MaterialOrder.objects.create(**validated_data)
-        for item_data in items_data:
-            MaterialOrderItem.objects.create(order=order, **item_data)
+        
+        for supplier_data in suppliers_data:
+            items_data = supplier_data.pop('items', [])
+            supplier = MaterialOrderSupplier.objects.create(order=order, **supplier_data)
+            for item_data in items_data:
+                MaterialOrderItem.objects.create(supplier=supplier, **item_data)
+                
         return order
 
     def update(self, instance, validated_data):
-        items_data = validated_data.pop('items', None)
+        suppliers_data = validated_data.pop('suppliers', None)
         
         instance.description = validated_data.get('description', instance.description)
         instance.status = validated_data.get('status', instance.status)
+        instance.expected_date = validated_data.get('expected_date', instance.expected_date)
+        instance.delivery_address = validated_data.get('delivery_address', instance.delivery_address)
         instance.save()
 
-        if items_data is not None:
-            instance.items.all().delete()
-            for item_data in items_data:
-                MaterialOrderItem.objects.create(order=instance, **item_data)
+        if suppliers_data is not None:
+            # Recreate suppliers and items
+            instance.suppliers.all().delete()
+            for supplier_data in suppliers_data:
+                items_data = supplier_data.pop('items', [])
+                supplier = MaterialOrderSupplier.objects.create(order=instance, **supplier_data)
+                for item_data in items_data:
+                    MaterialOrderItem.objects.create(supplier=supplier, **item_data)
                 
         return instance
